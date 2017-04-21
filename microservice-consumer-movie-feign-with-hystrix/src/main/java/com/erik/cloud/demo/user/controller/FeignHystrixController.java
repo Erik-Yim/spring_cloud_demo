61e4fad719d4e3ba314cf19e63463a2890330dae
Erik Yim <erik_yim@aliyun.com>
2017-04-21 09:45:30 +0800

    修改信息

--- microservice-consumer-movie-feign-with-hystrix/src/main/java/com/erik/cloud/demo/user/controller/FeignHystrixController.java
+++ microservice-consumer-movie-feign-with-hystrix/src/main/java/com/erik/cloud/demo/user/controller/FeignHystrixController.java
@@ -3,12 +3,15 @@ package com.erik.cloud.demo.user.controller;
 import com.erik.cloud.demo.user.entity.User;
 import com.erik.cloud.demo.user.feign.UserFeignHystrixClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FeignHystrixController {
+ // @Qualifier("microservice-provider-user")
+ // @Qualifier("hystrixClientFallback")
   @Autowired
   private UserFeignHystrixClient userFeignHystrixClient;
 
