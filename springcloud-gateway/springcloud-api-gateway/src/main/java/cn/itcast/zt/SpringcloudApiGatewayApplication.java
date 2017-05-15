d8f4431d5c0c3fe1527d0addda62961642c99ad4
Erik Yim <erik_yim@aliyun.com>
2017-05-15 13:31:46 +0800

    SpringCloud ribbon 调用服务路由网关Zuul

--- springcloud-gateway/springcloud-api-gateway/src/main/java/cn/itcast/zt/SpringcloudApiGatewayApplication.java
+++ springcloud-gateway/springcloud-api-gateway/src/main/java/cn/itcast/zt/SpringcloudApiGatewayApplication.java
@@ -1,8 +1,10 @@
 package cn.itcast.zt;
 
+import cn.itcast.zt.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 参考文章：介绍服务网关，地址绑定以及权限过滤路由
@@ -13,10 +15,13 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 public class SpringcloudApiGatewayApplication {
 
-	/*@Bean
+
+	//ribbon调用路由的时候传递token过来了，所以过滤器可以，不需要注解这个
+	//但是用feign暂时不能通过url传递其他参数过来和httpHeader过来所以那个demo需要注释这个filter
+	@Bean
 	public AccessFilter accessFilter() {
 		return new AccessFilter() ;
-	}*/
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudApiGatewayApplication.class, args);
