5b8f78bce0f8423d55ebdc3cd8e99e770954851a
Erik Yim <erik_yim@aliyun.com>
2017-05-16 16:48:49 +0800

    SpringCloud ribbon 调用服务路由网关Zuul

--- springcloud-gateway/springcloud-consumer-b/src/main/java/cn/itcast/zt/SpringcloudConsumerBApplication.java
+++ springcloud-gateway/springcloud-consumer-b/src/main/java/cn/itcast/zt/SpringcloudConsumerBApplication.java
@@ -1,14 +1,21 @@
 package cn.itcast.zt;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
 public class SpringcloudConsumerBApplication {
+
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.BASIC;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudConsumerBApplication.class, args);
---
+++ springcloud-gateway/springcloud-consumer-b/src/main/java/cn/itcast/zt/web/ConsumerBVO.java
@@ -0,0 +1,35 @@
+package cn.itcast.zt.web;
+
+/**
+ * Created by Erik_Yim on 2017/5/16.
+ */
+public class ConsumerBVO
+{
+    private Integer a;
+    private Integer b;
+    private String accessToken;
+
+    public Integer getA() {
+        return a;
+    }
+
+    public void setA(Integer a) {
+        this.a = a;
+    }
+
+    public Integer getB() {
+        return b;
+    }
+
+    public void setB(Integer b) {
+        this.b = b;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
