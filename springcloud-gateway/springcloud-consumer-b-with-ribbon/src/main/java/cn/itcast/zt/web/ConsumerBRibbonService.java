bd1eae346a74ff1eadef1211719fb5d6459a0a47
Erik Yim <erik_yim@aliyun.com>
2017-05-16 16:22:48 +0800

    SpringCloud ribbon 调用服务路由网关Zuul

--- springcloud-gateway/springcloud-consumer-b-with-ribbon/src/main/java/cn/itcast/zt/web/ConsumerBRibbonService.java
+++ springcloud-gateway/springcloud-consumer-b-with-ribbon/src/main/java/cn/itcast/zt/web/ConsumerBRibbonService.java
@@ -12,7 +12,8 @@ public class ConsumerBRibbonService {
   public String consumerB(Integer a, Integer b) {
     // http://服务提供者的serviceId/url,ribbon 可以传递很多参数比如httpheader 所以方便集成jwt，oauth2类似我们使用httpClient 去调用
     //web service
-    //TODO 了解下RestTemplate 还有Jersey，RESTEasy的rest框架
-    return this.restTemplate.getForObject("http://api-gateway/api-b/add?accessToken=xxxx&a=" + a + "&b=" + b, String.class);
+    //TODO 了解下RestTemplate 还有Jersey，RESTEasy的rest框架,{1}占位符，还可以使用{key}, 然后参数传入map对应key就会拼上去
+
+    return this.restTemplate.getForObject("http://api-gateway/api-b/add?accessToken={1}&a=" + a + "&b=" + b, String.class,"tokenValue");
   }
 }
--- springcloud-gateway/springcloud-consumer-b/src/main/java/cn/itcast/zt/web/ServiceBFeignClient.java
+++ springcloud-gateway/springcloud-consumer-b/src/main/java/cn/itcast/zt/web/ServiceBFeignClient.java
@@ -1,17 +1,30 @@
 package cn.itcast.zt.web;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Erik_Yim on 2017/5/12.
  */
-@FeignClient(name = "api-gateway")
+@FeignClient(name = "api-gateway") //name 不区分大小写
 public interface ServiceBFeignClient {
 
 
     @RequestMapping("/api-b/add")
     //使用feign一定要使用RequestParam并且要写名字，或者PathVariable一定要加名称不然会报错
+    //feign参数的内容就是相当于http请求中参数和header，和我要调用的方法的参数可以不一样
+    //之前的那个filter需要传accessToken 可以直接在方法里加上@RequestParam("AccessToken") String accessToken,api-gateway 就能取到token了
+    //这里面的方法签名不需要和调用的方法签名一样，可以多一些就行了，但是参数名称是一样的，原理就是把这些值和名称放到http request中
     public String consumerB(@RequestParam("a") Integer a, @RequestParam("b") Integer b);
+
+    //feign 使用教程@RequestHeader("accessToken")和@RequestParam("a")参数不能少，通过这个把http的请求参数名称拼接到URI中
+    //下面这个方法的解释为调用/api-b/add使用get方法发送，在http中加入请求参数a和b，并且加上请求头accessToken
+    @RequestMapping(value="/api-b/add", method = RequestMethod.GET)
+    public String consumerB2(@RequestHeader("accessToken") String accessToken, @RequestParam("a") Integer a, @RequestParam("b") Integer b);
+
+
+    //feign 传递复杂对象，其实就是包装一层，然后外面代理，把参数解析按照你的mapping和参数的类型在使用RestTemplate 传到后台去
+    //这个方法签名和你调用的api的方法签名不一样的，这是两个方法，并不是同一个方法
+    @RequestMapping(value="/api-b/add", method = RequestMethod.GET)
+    public String consumerB2(@RequestBody ConsumerBVO consumerBVO);
 }
