921f2e5ac5010e4aae9a6f72f6766f7f081087fa
Erik Yim <erik_yim@aliyun.com>
2017-05-16 13:41:06 +0800

    SpringCloud ribbon 调用服务路由网关Zuul

--- springcloud-gateway/springcloud-api-gateway/src/main/java/cn/itcast/zt/SpringcloudApiGatewayApplication.java
+++ springcloud-gateway/springcloud-api-gateway/src/main/java/cn/itcast/zt/SpringcloudApiGatewayApplication.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 
 /**
  * 参考文章：介绍服务网关，地址绑定以及权限过滤路由
+ *
+ * 在filter中集成spring security或者jwt,oauth2等实现保护接口安全访问。
+ *
  * http://blog.didispace.com/springcloud5/
  */
 //@SpringBootApplication
